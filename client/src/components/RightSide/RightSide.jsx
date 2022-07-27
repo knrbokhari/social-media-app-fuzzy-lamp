56f1bc563f90d12b227b6f1f582bcbf4b681d22c
Kazi Naeem Rayhan <naeem@pop-os.localdomain>
2022-07-27 21:56:01 +0600

    creating nav icons

--- client/src/components/RightSide/RightSide.jsx
+++ client/src/components/RightSide/RightSide.jsx
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import "./RightSide.css";
+import { AiFillHome, AiOutlineComment } from "react-icons/ai";
+import { MdOutlineNotifications, MdSettings } from "react-icons/md";
 
 const RightSide = () => {
-  return <div>RightSide</div>;
+  const [modalOpened, setModalOpened] = useState(false);
+  return (
+    <div className="RightSide">
+      <div className="navIcons">
+        <AiFillHome
+          style={{
+            fontSize: "25px",
+            color: "#f95f35",
+            cursor: "pointer",
+          }}
+        />
+        <MdSettings
+          style={{
+            fontSize: "25px",
+            color: "#f95f35",
+            cursor: "pointer",
+          }}
+        />
+        <MdOutlineNotifications
+          style={{
+            fontSize: "25px",
+            color: "#f95f35",
+            cursor: "pointer",
+          }}
+        />
+        <AiOutlineComment
+          style={{
+            fontSize: "25px",
+            color: "#f95f35",
+            cursor: "pointer",
+          }}
+        />
+      </div>
+
+      <button className="button r-button" onClick={() => setModalOpened(true)}>
+        Share
+      </button>
+    </div>
+  );
 };
 
 export default RightSide;
