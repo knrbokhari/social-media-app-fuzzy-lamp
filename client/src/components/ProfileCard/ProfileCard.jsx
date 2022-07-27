3a18d3be643d46a9f592573ec170ad40037194d4
Kazi Naeem Rayhan <naeem@pop-os.localdomain>
2022-07-27 17:54:10 +0600

    creating profileCard

---
+++ client/src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import "./ProfileCard.css";
+import cover from "../../img/cover.jpg";
+import profile from "../../img/profileImg.jpg";
+
+const ProfileCard = () => {
+  return (
+    <div className="ProfileCard">
+      <div className="ProfileInames">
+        <img src={cover} alt="" />
+        <img src={profile} alt="" />
+      </div>
+      <div className="ProfileName">
+        <span className="Name">Name</span>
+        <span className="ProfileTitle">Full Stack Developer</span>
+      </div>
+
+      <div className="FollowStatus">
+        <hr />
+        <div>
+          <div className="follow">
+            <span className="FollowingsNumber">6,100</span>
+            <span>Followings</span>
+          </div>
+          <div className="vl"></div>
+          <div className="follow">
+            <span className="FollowersNumber">0</span>
+            <span>Followers</span>
+          </div>
+        </div>
+        <hr />
+      </div>
+      <span>My Profile</span>
+    </div>
+  );
+};
+
+export default ProfileCard;
