33b6930ff09716a6b68caa64580f24bd8222915f
Kazi Naeem Rayhan <naeem@pop-os.localdomain>
2022-07-27 08:59:06 +0600

    creating Home page

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="profileSite">Profile</div>
+      <div className="postSite">Post</div>
+      <div className="rightSite">Right Side</div>
+    </div>
+  );
+};
+
+export default Home;
