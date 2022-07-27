d5c370030157d3345353470589320aca0541fb05
Kazi Naeem Rayhan <naeem@pop-os.localdomain>
2022-07-27 22:01:45 +0600

    creating TreadCard

--- client/src/components/RightSide/RightSide.jsx
+++ client/src/components/RightSide/RightSide.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./RightSide.css";
 import { AiFillHome, AiOutlineComment } from "react-icons/ai";
 import { MdOutlineNotifications, MdSettings } from "react-icons/md";
+import TrendCard from "../TrendCard/TrendCard";
 
 const RightSide = () => {
   const [modalOpened, setModalOpened] = useState(false);
@@ -37,6 +38,8 @@ const RightSide = () => {
           }}
         />
       </div>
+
+      <TrendCard />
 
       <button className="button r-button" onClick={() => setModalOpened(true)}>
         Share
