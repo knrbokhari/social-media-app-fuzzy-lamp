a887a5930983e4be4d66a950201e493db9f7e56c
Kazi Naeem Rayhan <naeem@pop-os.localdomain>
2022-07-27 18:21:56 +0600

    creating FollowersCard

--- client/src/components/FollowersCard/FollowersCard.jsx
+++ client/src/components/FollowersCard/FollowersCard.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import "./FollowersCard.css";
+import { Followers } from "../../Data/FollowersData";
 
 const FollowersCard = () => {
   return (
     <div className="FollowersCard">
       <h3>Who is following you</h3>
+      {Followers.map((follower) => {
+        return (
+          <div className="follower">
+            <div>
+              <img
+                src={follower.img}
+                alt="followerImg"
+                className="followerImage"
+              />
+              <div className="name">
+                <span>{follower.name}</span>
+                <span>@{follower.username}</span>
+              </div>
+            </div>
+            <button className="button fc-button" type="submit">
+              Follow
+            </button>
+          </div>
+        );
+      })}
     </div>
   );
 };
