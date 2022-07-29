eb8d9c8b985646acd82cb3c3c7632fd7babda73e
Kazi Naeem Rayhan <naeem@pop-os.localdomain>
2022-07-29 22:57:27 +0600

    find user from db

--- server/Routes/UserRoute.js
+++ server/Routes/UserRoute.js
@@ -1,7 +1,9 @@
 import express from "express";
+import { getUser } from "../Controllers/UserController";
 
 const router = express.Router();
 
-
+// get user from db
+router.get("/:id", getUser);
 
 export default router;
