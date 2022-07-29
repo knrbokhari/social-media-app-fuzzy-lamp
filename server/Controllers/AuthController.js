bee04c1df907a4f86de5879564af213c8b2eec41
Kazi Naeem Rayhan <naeem@pop-os.localdomain>
2022-07-29 16:56:43 +0600

    creating register route & registerUser controller

---
+++ server/Controllers/AuthController.js
@@ -0,0 +1,23 @@
+import UserModel from "../Models/UserModel.js";
+import bcrypt from "bcryptjs";
+
+// Register a new User
+export const registerUser = async (req, res) => {
+  const { username, password, firstname, lastname } = req.body;
+  const salt = await bcrypt.genSalt(10);
+  const hashedPass = await bcrypt.hash(password, salt);
+
+  const newUser = new UserModel({
+    username,
+    password: hashedPass,
+    firstname,
+    lastname,
+  });
+
+  try {
+    await newUser.save();
+    res.status(200).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
