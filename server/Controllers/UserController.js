9c4f8cc2cf17d8475b4c727d55bb273053a15ef7
Kazi Naeem Rayhan <naeem@pop-os.localdomain>
2022-07-29 23:02:09 +0600

    update user data

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -19,3 +19,28 @@ export const getUser = async (req, res) => {
     res.status(500).json(error);
   }
 };
+
+// update a user
+export const updateUser = async (req, res) => {
+  const id = req.params.id;
+  const { currentUserId, currentUserAdminStatus, password } = req.body;
+
+  if (id === currentUserId || currentUserAdminStatus) {
+    try {
+      if (password) {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(password, salt);
+      }
+
+      const user = await UserModel.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  } else {
+    res.status(403).json("Access Denied! you can only update your own profile");
+  }
+};
--- server/Routes/UserRoute.js
+++ server/Routes/UserRoute.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { getUser } from "../Controllers/UserController";
+import { getUser, updateUser } from "../Controllers/UserController";
 
 const router = express.Router();
 
 // get user from db
 router.get("/:id", getUser);
+
+// update a user
+router.put("/:id", updateUser);
 
 export default router;
