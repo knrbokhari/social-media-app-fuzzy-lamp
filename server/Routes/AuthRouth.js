e9765810e3435fe462c694f3411ee7748e5b4f1a
Kazi Naeem Rayhan <naeem@pop-os.localdomain>
2022-07-29 17:30:05 +0600

    creating login route & loginUser controller

--- server/Routes/AuthRouth.js
+++ server/Routes/AuthRouth.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { registerUser } from "../Controllers/AuthController.js";
+import { loginUser, registerUser } from "../Controllers/AuthController.js";
 
 const router = express.Router();
 
+// register User
 router.post("/register", registerUser);
+// login User
+router.post("/register", loginUser);
 
 export default router;
