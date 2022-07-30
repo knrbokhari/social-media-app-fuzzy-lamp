30780754a786a00499dca71ea30dac59386283be
Kazi Naeem Rayhan <naeem@pop-os.localdomain>
2022-07-30 18:28:58 +0600

    updating register & login from

--- client/src/pages/Auth/Auth.jsx
+++ client/src/pages/Auth/Auth.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./Auth.css";
 import Logo from "../../img/logo.png";
+import { useState } from "react";
 
 const Auth = () => {
+  const [isSignUp, setIsSignUp] = useState(false);
   return (
     <div className="Auth">
+      {/* left side */}
       <div className="a-left">
         <img src={Logo} alt="" />
         <div className="Webname">
@@ -13,101 +16,171 @@ const Auth = () => {
         </div>
       </div>
 
-      {/* <LogIn /> */}
-      <SignUp />
+      {/* right side */}
+      <div className="a-right">
+        <form className="infoForm authForm">
+          <h3>{isSignUp ? "Register" : "Login"}</h3>
+          {isSignUp && (
+            <div>
+              <input
+                required
+                type="text"
+                placeholder="First Name"
+                className="infoInput"
+                name="firstname"
+              />
+              <input
+                required
+                type="text"
+                placeholder="Last Name"
+                className="infoInput"
+                name="lastname"
+              />
+            </div>
+          )}
+
+          <div>
+            <input
+              type="text"
+              className="infoInput"
+              name="username"
+              placeholder="Usernames"
+            />
+          </div>
+
+          <div>
+            <input
+              type="text"
+              className="infoInput"
+              name="password"
+              placeholder="Password"
+            />
+            {isSignUp && (
+              <input
+                type="text"
+                className="infoInput"
+                name="confirmpass"
+                placeholder="Confirm Password"
+              />
+            )}
+          </div>
+
+          <div>
+            <span
+              style={{
+                fontSize: "12px",
+                cursor: "pointer",
+                textDecoration: "underline",
+              }}
+              onClick={() => {
+                setIsSignUp((prev) => !prev);
+              }}
+            >
+              {isSignUp
+                ? "Already have an account Login"
+                : "Don't have an account Sign up"}
+            </span>
+          </div>
+          <button className="button infoButton" type="submit">
+            {isSignUp ? "SignUp" : "Login"}
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
-function LogIn() {
-  return (
-    <div className="a-right">
-      <form className="infoForm authForm">
-        <h3>Log In</h3>
 
-        <div>
-          <input
-            type="text"
-            placeholder="Username"
-            className="infoInput"
-            name="username"
-          />
-        </div>
+// function LogIn() {
+//   return (
+//     <div className="a-right">
+//       <form className="infoForm authForm">
+//         <h3>Log In</h3>
 
-        <div>
-          <input
-            type="password"
-            className="infoInput"
-            placeholder="Password"
-            name="password"
-          />
-        </div>
+//         <div>
+//           <input
+//             type="text"
+//             placeholder="Username"
+//             className="infoInput"
+//             name="username"
+//           />
+//         </div>
 
-        <div>
-          <span style={{ fontSize: "12px" }}>
-            Don't have an account Sign up
-          </span>
-          <button className="button infoButton">Login</button>
-        </div>
-      </form>
-    </div>
-  );
-}
-function SignUp() {
-  return (
-    <div className="a-right">
-      <form className="infoForm authForm">
-        <h3>Sign up</h3>
+//         <div>
+//           <input
+//             type="password"
+//             className="infoInput"
+//             placeholder="Password"
+//             name="password"
+//           />
+//         </div>
 
-        <div>
-          <input
-            type="text"
-            placeholder="First Name"
-            className="infoInput"
-            name="firstname"
-          />
-          <input
-            type="text"
-            placeholder="Last Name"
-            className="infoInput"
-            name="lastname"
-          />
-        </div>
+//         <div>
+//           <span style={{ fontSize: "12px" }}>
+//             Don't have an account Sign up
+//           </span>
+//           <button className="button infoButton">Login</button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// }
 
-        <div>
-          <input
-            type="text"
-            className="infoInput"
-            name="username"
-            placeholder="Usernames"
-          />
-        </div>
+// function SignUp() {
+//   return (
+//     <div className="a-right">
+//       <form className="infoForm authForm">
+//         <h3>Sign up</h3>
 
-        <div>
-          <input
-            type="text"
-            className="infoInput"
-            name="password"
-            placeholder="Password"
-          />
-          <input
-            type="text"
-            className="infoInput"
-            name="confirmpass"
-            placeholder="Confirm Password"
-          />
-        </div>
+//         <div>
+//           <input
+//             type="text"
+//             placeholder="First Name"
+//             className="infoInput"
+//             name="firstname"
+//           />
+//           <input
+//             type="text"
+//             placeholder="Last Name"
+//             className="infoInput"
+//             name="lastname"
+//           />
+//         </div>
 
-        <div>
-          <span style={{ fontSize: "12px" }}>
-            Already have an account. Login!
-          </span>
-        </div>
-        <button className="button infoButton" type="submit">
-          Signup
-        </button>
-      </form>
-    </div>
-  );
-}
+//         <div>
+//           <input
+//             type="text"
+//             className="infoInput"
+//             name="username"
+//             placeholder="Usernames"
+//           />
+//         </div>
+
+//         <div>
+//           <input
+//             type="text"
+//             className="infoInput"
+//             name="password"
+//             placeholder="Password"
+//           />
+//           <input
+//             type="text"
+//             className="infoInput"
+//             name="confirmpass"
+//             placeholder="Confirm Password"
+//           />
+//         </div>
+
+//         <div>
+//           <span style={{ fontSize: "12px" }}>
+//             Already have an account. Login!
+//           </span>
+//         </div>
+//         <button className="button infoButton" type="submit">
+//           Signup
+//         </button>
+//       </form>
+//     </div>
+//   );
+// }
 
 export default Auth;
