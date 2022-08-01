f0b4bb82623e21b99d293cf6f54d6aed4fbe428e
Kazi Naeem Rayhan <naeem@pop-os.localdomain>
2022-08-01 16:00:29 +0600

    jwt token implemented

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -3,32 +3,34 @@ import bcrypt from "bcryptjs";
 
 // Register a new User
 export const registerUser = async (req, res) => {
-  const { username, password, firstname, lastname } = req.body;
   const salt = await bcrypt.genSalt(10);
-  const hashedPass = await bcrypt.hash(password, salt);
+  const hashedPass = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hashedPass;
+  const { username } = req.body;
 
+  // check UserName
   const checkUserName = await UserModel.findOne({ username: username });
   if (checkUserName) {
-    return res.status(409).send("Username Already Exists");
+    return res.status(400).json({ message: "User already exists" });
   }
 
-  const newUser = new UserModel({
-    username,
-    password: hashedPass,
-    firstname,
-    lastname,
-  });
+  const newUser = new UserModel(req.body);
 
   try {
-    await newUser.save();
-    res.status(200).json(newUser);
+    const user = await newUser.save();
+    // jwt token
+    const token = jwt.sign(
+      { username: user.username, id: user._id },
+      process.env.JWTKEY,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({ user, token });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
 // login User
-
 export const loginUser = async (req, res) => {
   const { username, password } = req.body;
 
@@ -38,9 +40,17 @@ export const loginUser = async (req, res) => {
     if (user) {
       const validity = await bcrypt.compare(password, user.password);
 
-      validity
-        ? res.status(200).json(user)
-        : res.status(400).json("Wrong Password");
+      if (!validity) {
+        res.status(400).json("Wrong Password");
+      } else {
+        // jwt token
+        const token = jwt.sign(
+          { username: user.username, id: user._id },
+          process.env.JWTKEY,
+          { expiresIn: "1h" }
+        );
+        res.status(200).json({ user, token });
+      }
     } else {
       res.status(404).json("User does not exists");
     }
