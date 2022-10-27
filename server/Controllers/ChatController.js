3416ec34f491976047f58750ca6662fbb9e69520
kazi naeem rayhan <kazinaeemrayhan@gmail.com>
2022-10-27 19:02:26 +0600

    creating chart & message route & controller

---
+++ server/Controllers/ChatController.js
@@ -0,0 +1,7 @@
+import ChatModel from "../Models/ChatModel.js";
+
+export const createChat = async (req, res) => {};
+
+export const userChats = async (req, res) => {};
+
+export const findChat = async (req, res) => {};
---
+++ server/Controllers/MessageController.js
@@ -0,0 +1,5 @@
+import MessageModel from "../Models/MessageModel.js";
+
+export const addMessage = async (req, res) => {};
+
+export const getMessages = async (req, res) => {};
